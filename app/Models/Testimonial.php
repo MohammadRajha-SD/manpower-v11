75a1cf3377835347808065efb4eebb1546236e95
MohammadRajha-SD <mohammadrajha2@gmail.com>
2025-08-08 02:46:04 +0300

    d

--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Testimonial extends Model
 {
     use HasFactory;
-    
+
     protected $guarded = [];
+
+    public $translatable = ['name', 'description'];
 }
